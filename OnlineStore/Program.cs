5e41faa01ffd89e1d17df9ac1ee1de0dbad60a0a
elain21 <retrohakalik@gmail.com>
2024-02-19 23:05:27 +0400

    Added database in DI

--- OnlineStore/Program.cs
+++ OnlineStore/Program.cs
@@ -1,9 +1,27 @@
+using Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+var databaseConnectionString = builder.Configuration.GetConnectionString("OnlineStoreContext");
+builder.Services.AddDbContext<OnlineStoreContext>(options =>
+{
+    options.UseNpgsql(
+        databaseConnectionString,
+        optionsBuilder =>
+        {
+            optionsBuilder.CommandTimeout(120);
+        }
+    );
+}, ServiceLifetime.Transient);
+
+builder.Services.AddTransient<OnlineStoreContext>();
 
 var app = builder.Build();
 
@@ -15,5 +33,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
